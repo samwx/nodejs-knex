e0c4727621cb0d5cd72cae3e0940ac722890b227
samwx <samuelmartins.sw@gmail.com>
2019-11-05 12:57:12 -0300

    Use new url parser option

--- store/Connection.js
+++ store/Connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectionString = 'your-connection-string'
 
-const openConnection = () => mongoose.connect(connectionString)
+const openConnection = () => mongoose.connect(connectionString, { useNewUrlParser: true })
 
 module.exports = {
     openConnection,
